2a33372093843ae7032601b55ef606acb027d727
abdoulayesedego <abdoulaye.sedego95@gmail.com>
2025-10-26 02:21:10 -0400

    set up web server and create home handler

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world update")
+
+	// the http.NewServeMux() function to initialize a new servemux, then
+	mux := http.NewServeMux()
+	// register the home function as the handler for the "/" URL pattern.
+	mux.HandleFunc("/", home)
+
+	//port for the web server to listen to
+	port := ":4000"
+
+	log.Println("Starting web server on port ", port)
+	//http.ListenAndServe() function start a new web server. take in
+	// two parameters: the TCP network address to listen on and the servemux
+	err := http.ListenAndServe(port, mux)
+
+	//If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit.
+	//any error returned by http.ListenAndServe() is always non-nil.
+	log.Fatal(err)
+
 }
